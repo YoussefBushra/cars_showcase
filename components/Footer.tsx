06464145bd9fa7d332cceb630ce8e81ac512f39e
Youssef Bushra <youssef190346@bue.edu.eg>
2023-09-26 03:13:27 +0300

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constants";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-200">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="logo.svg"
+            alt="Car Hub Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-500">
+            {" "}
+            CarHub 2023 <br /> All rights reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  className="text-gray-500"
+                  href={item.url}
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p className="text-base text-gray-500">
+          {" "}
+          @2023 CarHub. All rights reserved &copy;
+        </p>
+        <div className="footer__copyrights-link">
+          <Link href="/"> Privacy policy</Link>
+          <Link href="/"> Terms of use</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
